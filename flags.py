ee07a4f7295b73e320571e1a4d53579fe5813ce2
minoring <minho1350@gmail.com>
2019-12-04 12:31:25 -0800

    Convert image ratio

--- flags.py
+++ flags.py
@@ -5,8 +5,8 @@ def define_flags():
   flags.DEFINE_string('data_dir', 'data', 'Directory path of dataset')
   flags.DEFINE_string('URL', 'Green_Sea_Turtle_grazing_seagrass.jpg',
                       'URL of the image')
-  flags.DEFINE_integer('target_height', 240, 'Height of target image')
-  flags.DEFINE_integer('target_width', 320, 'Height of target width')
+  flags.DEFINE_integer('target_height', 225, 'Height of target image')
+  flags.DEFINE_integer('target_width', 375, 'Height of target width')
   flags.DEFINE_integer('num_steps', 300, 'Number of steps for training')
   flags.DEFINE_float('learning_rate', 0.001, 'Learning rate')
   flags.DEFINE_bool('scaling', True, 'Allow scaling up of the image')
